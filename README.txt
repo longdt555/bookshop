9c50a859a571290f0dedcf80e5149611a053adaa
LongDinh <longdt555@gmail.com>
2018-12-03 01:19:36 +0700

    longdt
    updated

--- README.txt
+++ README.txt
@@ -5,3 +5,7 @@ Hướng dẫn cài đặt môi trường
 Tạo virtualenv: virtualenv -p /usr/bin/python3 venv && source venv/bin/activate
 
 Cài đặt requirements: pip install -r requirements.txt
+
+
+
+https://eggclub.org/django-04-view-nang-cao/
--- book/views.py
+++ book/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View
 from django.template import loader
 # Create your views here.
 
 def index(request):
     template = loader.get_template('book/index.html')
     context = {
-        'name':'Yong',
+        'name':'world',
+        'yourname':'Yong',
     }
     return HttpResponse(template.render(context, request))
-
-#
-# def showName(request):
-#     template = loader.get_template('book/name.html')
-#     context= {
-#         'yourname':'Long'
-#     }
-#     return HttpResponse(template.render(context,request))
