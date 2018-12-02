cb7cf555de8520bc6e4659ef06022696ef05cf54
LongDinh <longdt555@gmail.com>
2018-12-03 01:01:01 +0700

    longdt
    updated code

--- book/views.py
+++ book/views.py
@@ -4,17 +4,17 @@ from django.views.generic import View
 from django.template import loader
 # Create your views here.
 
-def sayHello(request):
+def index(request):
     template = loader.get_template('book/index.html')
     context = {
-        'name':'World',
+        'name':'Yong',
     }
     return HttpResponse(template.render(context, request))
 
-
-def showName(request):
-    template = loader.get_template('book/name.html')
-    context= {
-        'yourname':'Long'
-    }
-    return HttpResponse(template.render(context,request))
+#
+# def showName(request):
+#     template = loader.get_template('book/name.html')
+#     context= {
+#         'yourname':'Long'
+#     }
+#     return HttpResponse(template.render(context,request))
