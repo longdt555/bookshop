50c43e0dc65998e20cbcaabad29f0ea404eadec2
LongDinh <longdt555@gmail.com>
2018-12-07 02:49:01 -0800

    LongDT
    Commit

--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,5 @@ urlpatterns=[
     url(r'^home/$', views.Home),
     url(r'^about/$', views.AboutMe),
     url(r'^search/$', views.SearchForm),
-    
+    url(r'^infor/$', views.ShowInfor),
 ]
--- book/views.py
+++ book/views.py
@@ -14,10 +14,40 @@ def AboutMe(request):
 
 def SearchForm(request):
     return render(request, 'book/search_form.html')
-
-def info(request):
-    if 'search' in request.GET:
-        message = 'You searched for: %r' % request.GET['search']
-    else
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
+#
+# def ShowInfor(request):
+#     if 'name' in request.GET:
+#         template = loader.get_template('book/infor.html')
+#         context={
+#             'name':request.GET['name'],
+#             'role':request.GET['role'],
+#             'level':request.GET['level'],
+#             'rank':request.GET['rank'],
+#         }
+#     else:
+#         return render(request, 'book/search_form.html')
+#     return HttpResponse(template.render(context, request))
+thisdict =	{
+  "name": "YONG",
+  "role": "M249",
+  "level": 54,
+  "rank":"Crown"
+}
+def ShowInfor(request):
+    errors=[]
+    if 'name' in request.GET:
+        name = request.GET['name']
+        if not name:
+            errors.append('Enter a search term.')
+        elif len(name) > 10:
+            errors.append('Please enter at most 20 characters.')
+        else:
+            template = loader.get_template('book/infor.html')
+            context={
+                'name':request.GET['name'],
+                'role' : thisdict["role"],
+                'level' : thisdict["level"],
+                'rank': thisdict["rank"]
+            }
+            return HttpResponse(template.render(context, request))
+    return render(request, 'book/search_form.html', {'errors':errors})
